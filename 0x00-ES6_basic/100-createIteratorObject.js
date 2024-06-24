1bef1caa1a1a821720c485bb0a017244a34636bf
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-24 18:02:56 +0000

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const department in report.allEmployees) {
+    if (report.allEmployees.hasOwnProperty(department)) {
+      allEmployees = allEmployees.concat(report.allEmployees[department]);
+    }
+  }
+  return allEmployees[Symbol.iterator]();
+}

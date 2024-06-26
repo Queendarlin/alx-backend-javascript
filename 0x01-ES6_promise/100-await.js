daa8a9d4a44bc3e0e554dba6540f09f0001fe08a
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-26 10:49:01 +0000

    An async function named asyncUploadUser that will call two functions and return an object

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}

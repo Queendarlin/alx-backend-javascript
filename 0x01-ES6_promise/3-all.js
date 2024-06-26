bca2d1c1cfd94de9fb86df1ae68b8742c913b399
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-26 09:09:00 +0000

    Using Promise.all to wait for two asynchronous operations (uploadPhoto and createUser) to complete

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}

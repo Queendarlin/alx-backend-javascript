39cada5a1022c5702bfa110435d71123d068da3a
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-26 10:25:08 +0000

    Using throw, try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    }
+    return numerator / denominator;
+  } catch (error) {
+    throw error;
+  }
+}

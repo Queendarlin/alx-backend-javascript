05db284d84d8cb6606a482de9e9d5eacc8900bdf
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-27 17:21:24 +0000

    Privacy Considerations.

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,21 +1,12 @@
-const cloneSymbol = Symbol('clone');
-
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  // Method to clone the car
-  [cloneSymbol]() {
-    const Constructor = this.constructor;
-    return new Constructor(this._brand, this._motor, this._color);
-  }
-
   cloneCar() {
-    return this[cloneSymbol]();
+    const Species = this.constructor[Symbol.species] || this.constructor;
+    return new Species();
   }
 }
-
-export default Car;

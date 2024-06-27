52cd0490e7c3f56620c94bd784bc6b05ecd34cc4
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-27 15:28:56 +0000

    Adding static method

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -6,12 +6,10 @@ export default class Pricing {
     this.currency = currency;
   }
 
-  // Getter for amount
   get amount() {
     return this._amount;
   }
 
-  // Setter for amount
   set amount(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Amount must be a number');
@@ -19,12 +17,10 @@ export default class Pricing {
     this._amount = value;
   }
 
-  // Getter for currency
   get currency() {
     return this._currency;
   }
 
-  // Setter for currency
   set currency(value) {
     if (!(value instanceof Currency)) {
       throw new TypeError('Currency must be an instance of Currency');
@@ -32,12 +28,10 @@ export default class Pricing {
     this._currency = value;
   }
 
-  // Method to display the full price
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
-  // Static method to convert the price
   static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
       throw new TypeError('Amount and conversion rate must be numbers');

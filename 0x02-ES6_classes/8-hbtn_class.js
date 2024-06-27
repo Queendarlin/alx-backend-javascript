df2c97b4045f571639141a46e70721ccf1a02dfd
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-27 16:24:14 +0000

    Overide toString method and valueOf method

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // Override valueOf method to return the size
+  valueOf() {
+    return this._size;
+  }
+
+  // Override toString method to return the location
+  toString() {
+    return this._location;
+  }
+}

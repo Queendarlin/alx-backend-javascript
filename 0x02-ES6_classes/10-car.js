fe8ffea32ea3bbf7f5e316d0eefd4a770b5f957b
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-27 17:01:29 +0000

    Class with a method to return a new object of the class.

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const cloneSymbol = Symbol('clone');
+
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Method to clone the car
+  [cloneSymbol]() {
+    const Constructor = this.constructor;
+    return new Constructor(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
+
+export default Car;

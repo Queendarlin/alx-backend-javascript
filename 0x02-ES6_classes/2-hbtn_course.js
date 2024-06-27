3738aede0e349db595a98bd46c4acc75cda4a2eb
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-27 12:23:38 +0000

    Using getters and setters methods

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,12 +5,10 @@ export default class HolbertonCourse {
     this.students = students;
   }
 
-  // Getter for name
   get name() {
     return this._name;
   }
 
-  // Setter for name
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -18,12 +16,10 @@ export default class HolbertonCourse {
     this._name = value;
   }
 
-  // Getter for length
   get length() {
     return this._length;
   }
 
-  // Setter for length
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -31,12 +27,10 @@ export default class HolbertonCourse {
     this._length = value;
   }
 
-  // Getter for students
   get students() {
     return this._students;
   }
 
-  // Setter for students
   set students(value) {
     if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');

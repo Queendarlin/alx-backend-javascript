5f7c3b023e9604b3a93ed270e56c2d5518ece575
Queendarlin <queendarlinnnamani@gmail.com>
2024-06-27 17:33:21 +0000

    Update Readme

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,13 +1,21 @@
-import Car from './10-car';
+const cloneSymbol = Symbol('clone');
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  // eslint-disable-next-line
+  // Method to clone the car
+  [cloneSymbol]() {
+    const Constructor = this.constructor;
+    return new Constructor(this._brand, this._motor, this._color);
+  }
+
   cloneCar() {
-    return new Car[Symbol.species]();
+    return this[cloneSymbol]();
   }
 }
+
+export default Car;

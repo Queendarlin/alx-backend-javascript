a7be07438607f3bea4072d1a027b56386b26721a
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-01 18:33:02 +0000

    Using typedArray

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  view.setInt8(position, value);
+
+  return view;
+}
+
+export default createInt8TypedArray;

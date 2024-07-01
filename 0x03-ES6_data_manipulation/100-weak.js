2c557341ee9cb199491c8df7e58d0df9dc97aec3
Queendarlin <queendarlinnnamani@gmail.com>
2024-07-01 19:04:19 +0000

    Using weakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  let count = weakMap.get(endpoint);
+  count++;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { queryAPI, weakMap };

1e29229b1fb798f9606928ed7e3efaf4708983af
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-20 10:44:20 +0100

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const http = require('http')
+port = 1245;
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-Type': 'text/plain' })
+
+    res.end('Hello Holberton School!');
+})
+
+app.listen(port, () => {
+    console.log('Server is listening on port 1245');
+})
+
+module.exports = app
+

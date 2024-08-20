3ab624b286bdc4485de758cd522c0a8e699d331e
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-20 10:07:07 +0100

    Using Process Stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+process.stdin.on('data', (chunk) => {
+  const name = chunk.trim(); // Convert input to a string and remove any extra spaces/newlines
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {

9b7d93f3e3a6f2596d48a29cf2999fec500bf83c
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-20 09:36:05 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+// Display the initial message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Listen to the standard input (stdin) data event
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+
+// Listen to the standard input (stdin) end event
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});

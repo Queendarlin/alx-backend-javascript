4886f172a9063f3d0c05ea84d37ae00b055ca459
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-08-20 09:57:18 +0100

    Using Process StdIn

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
-// Display the welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input on stdin
-process.stdin.on('data', (data) => {
-  // Trim any extra newline characters from the input
-  const name = data.toString().trim();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-  // Display the user's name
-  process.stdout.write(`Your name is: ${name}\n`);
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
 
-  // Close stdin and print the closing message
-  process.stdin.end(() => {
-    process.stdout.write('This important software is now closing\n');
-  });
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
